e37a09fc01ecc758427cd2353b2e0cb4640aa35b
vishal lokhande <lvishal108@gmail.com>
2024-10-09 14:41:46 +0000

    pokedesk day 3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 // console.log("connected")
 
 import data from "./data.json" ;
-console.log(data)
+// console.log(data)
 
-for(let object of data){
-    console.log(object.name)
-}
+
+const inputEl = document.querySelector("#floatingInputGroup1");
+console.log(inputEl);
+// for(let object of data){
+//     console.log(object.name)
+// }
+document.addEventListener("keyup", (input) =>{
+   if(input.key === "/"){
+   inputEl.focus();
+   }
+});
