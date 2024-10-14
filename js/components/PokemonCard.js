e15529388f4466d25d518dc3e429f60752adf619
vishal lokhande <lvishal108@gmail.com>
2024-10-14 14:51:59 +0000

    add pokemon card component

---
+++ js/components/PokemonCard.js
@@ -0,0 +1,18 @@
+export default function PokemonCard(name, image, description, link) {
+  const div = document.createElement("div");
+  div.classList.add("col");
+  div.innerHTML = `<div class="card">
+              <img
+                src="${image}"
+                class="card-img-top"
+                alt="${name}
+              />
+              <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">${description}</p>
+  
+                <a href="${link}" class="btn btn-warning" target="_blank">visit</a>
+              </div>
+            </div>`;
+  return div;
+}
--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@
 import data from "./data.json";
 // console.log(data)
 
+import PokemonCard from "./components/PokemonCard";
+
 const inputEl = document.querySelector("input");
 console.log(inputEl);
 // for(let object of data){
@@ -13,34 +15,19 @@ const pokemonRow = document.querySelector("[data-pokemon-row]");
 //iterate over pokemon data
 
 for (let obj of data) {
-  const { name, image, description } = obj; //destructured
-  console.log(name);
-  // step 1= create a paragraph
-  const paragraph = document.createElement("p");
+  const { name, image, description, link} = obj; //destructured
+//   console.log(name);
+//   // step 1= create a paragraph
+//   const paragraph = document.createElement("p");
 
-  // step 2 = paragraph ka content (pokemon ka nam)
+//   // step 2 = paragraph ka content (pokemon ka nam)
 
-  paragraph.textContent = name;
-  // step 3 = paragraph ko pokemon mein me add karna hai
-  pokemonRow.appendChild(pokemonCard(name, image, description));
+//   paragraph.textContent = name;
+//   // step 3 = paragraph ko pokemon mein me add karna hai
+  pokemonRow.appendChild(PokemonCard(name, image, description, link));
 }
 
-function pokemonCard(name, image, description) {
-  const div = document.createElement("div");
-  div.classList.add("col");
-  div.innerHTML = `<div class="card">
-            <img
-              src="${image}"
-              class="card-img-top"
-              alt="${name}
-            />
-            <div class="card-body">
-              <h5 class="card-title">${name}</h5>
-              <p class="card-text">${description}</p>
-            </div>
-          </div>`;
-  return div;
-}
+
 
 document.addEventListener("keyup", (e) => {
   if (e.key === "/") {
