abc554732b9380a9941ad1c54f2924c228d3d440
vishal lokhande <lvishal108@gmail.com>
2024-10-15 14:39:11 +0000

    implement filter functionality

--- js/main.js
+++ js/main.js
@@ -14,24 +14,61 @@ const pokemonRow = document.querySelector("[data-pokemon-row]");
 
 //iterate over pokemon data
 
-for (let obj of data) {
-  const { name, image, description, link} = obj; //destructured
-//   console.log(name);
-//   // step 1= create a paragraph
-//   const paragraph = document.createElement("p");
+// render
 
-//   // step 2 = paragraph ka content (pokemon ka nam)
+function renderPokemons(list) {
+  // empty the previous content
 
-//   paragraph.textContent = name;
-//   // step 3 = paragraph ko pokemon mein me add karna hai
-  pokemonRow.appendChild(PokemonCard(name, image, description, link));
+  pokemonRow.innerHTML = "";
+
+  for (let obj of list) {
+    const { name, image, description, link } = obj;
+    const pokemon = PokemonCard(name, image, description, link);
+    pokemonRow.appendChild(pokemon);
+  }
 }
+renderPokemons(data);
 
+// filter functionality
 
+inputEl.addEventListener("input", (e) => {
+  const currentInput = e.target.value.toLowerCase().trim();
+
+  const filteredPokemons = data.filter((obj) =>
+    obj.name.toLowerCase().includes(currentInput)
+  );
+
+  if (filteredPokemons.length === 0) {
+    renderPokemons([
+      {
+        name: "Not found",
+        image:
+          "https://img.freepik.com/premium-vector/adorable-cartoon-panda-astronaut-soaring-through-space-rocket_1263357-29898.jpg",
+        description: " matches not found",
+        link: "https://pokemon.com",
+      },
+    ]);
+    return;
+  }
+
+  renderPokemons(filteredPokemons);
+});
+// for (let obj of data) {
+//   const { name, image, description, link} = obj; //destructured
+// //   console.log(name);
+// //   // step 1= create a paragraph
+// //   const paragraph = document.createElement("p");
+
+// //   // step 2 = paragraph ka content (pokemon ka nam)
+
+// //   paragraph.textContent = name;
+// //   // step 3 = paragraph ko pokemon mein me add karna hai
+//   pokemonRow.appendChild(PokemonCard(name, image, description, link));
+// }
 
 document.addEventListener("keyup", (e) => {
   if (e.key === "/") {
-    console.log(`slas hwas pressed`);
+    // console.log(`slas hwas pressed`);
     inputEl.focus();
   }
 });
